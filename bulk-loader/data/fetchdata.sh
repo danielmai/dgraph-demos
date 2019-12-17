e7f1c2d485ed779cacea534e87c13f98a3efd02a
Daniel Mai <daniel@dgraph.io>
2019-12-16 16:51:31 -0800

    Fix fetchdata.sh.

--- bulk-loader/data/fetchdata.sh
+++ bulk-loader/data/fetchdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Download 21million movie data and schema from the dgraph benchmarks repo master branch.
-wget -O 21million.rdf.gz https://github.com/dgraph-io/benchmarks/raw/master/data/release/release.schema
+wget -O 21million.rdf.gz https://github.com/dgraph-io/benchmarks/raw/master/data/release/21million.rdf.gz
+wget -O 21million.schema https://github.com/dgraph-io/benchmarks/raw/master/data/release/release.schema
