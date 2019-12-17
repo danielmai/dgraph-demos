648c296a8e6e387821bcb711a484744e141104c5
Daniel Mai <daniel@dgraph.io>
2019-12-16 16:46:31 -0800

    Update bulk loader demo for benchmarks repo master branch data set.

--- bulk-loader/data/fetchdata.sh
+++ bulk-loader/data/fetchdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Download 21million movie data and schema from the dgraph benchmarks repo release/v1.0 branch.
-wget -O 21million.rdf.gz https://github.com/dgraph-io/benchmarks/raw/release/v1.0/data/release/21million.rdf.gz
-wget -O 21million.schema https://github.com/dgraph-io/benchmarks/raw/release/v1.0/data/release/release.schema
+# Download 21million movie data and schema from the dgraph benchmarks repo master branch.
+wget -O 21million.rdf.gz https://github.com/dgraph-io/benchmarks/raw/master/data/release/release.schema
